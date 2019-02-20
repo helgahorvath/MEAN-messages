03ac5a12d94b18ed239e896cff211c4c7fd4da71
helgahorvath <h.horvath302@gmail.com>
2019-02-20 12:17:57 +0100

    post list component and html added

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {Post} from '../post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: 'post-list.component.html'
+})
+export class PostListComponent {
+  @Input() posts: Post[] = [];
+  // can be bound from its parent
+
+
+}
